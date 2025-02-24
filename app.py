a7589c9225729d86dec293784adcd7e2621cba1d
Amr2087 <amr2082002@gmail.com>
2025-02-24 14:43:36 +0200

    First

--- app.py
+++ app.py
@@ -242,4 +242,4 @@ def history():
     return jsonify({"chat_history": chat_history})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
